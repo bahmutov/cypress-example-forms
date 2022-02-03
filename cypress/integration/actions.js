018f3001a871e5ac3f4c9f86bcc80fa9ae38c996
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-02-03 17:53:35 -0500

    add example using cypress-react-app-actions

---
+++ cypress/integration/actions.js
@@ -0,0 +1,116 @@
+/// <reference types="cypress" />
+import 'cypress-react-app-actions'
+
+const typeOptions = { delay: 0 }
+
+describe('3 shorter tests', () => {
+  const startOfSecondPageState = {
+    currentStep: 2,
+    email: 'my-email@foo.bar',
+    field1a: 'Field 1a text value',
+    field1b: 'Field 1b text value',
+    field1c: 'Field 1c text value',
+    field1d: 'Field 1d text value',
+    field1e: 'Field 1e text value',
+    first: 'Joe',
+    last: 'Smith',
+    username: '',
+  }
+
+  const startOfThirdPageState = {
+    ...startOfSecondPageState,
+    currentStep: 3,
+    username: 'JoeSmith',
+    field2a: 'Field 2a text value',
+    field2b: 'Field 2b text value',
+    field2c: 'Field 2c text value',
+    field2d: 'Field 2d text value',
+    field2e: 'Field 2e text value',
+    field2f: 'Field 2f text value',
+    field2g: 'Field 2g text value',
+  }
+
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('first page', () => {
+    cy.log('First page')
+    cy.contains('h1', 'Book Hotel 1')
+
+    cy.get('#first').type('Joe', typeOptions)
+    cy.get('#last').type('Smith', typeOptions)
+    cy.get('#email').type('my-email@foo.bar', typeOptions)
+
+    cy.get('#field1a').type('Field 1a text value', typeOptions)
+    cy.get('#field1b').type('Field 1b text value', typeOptions)
+    cy.get('#field1c').type('Field 1c text value', typeOptions)
+    cy.get('#field1d').type('Field 1d text value', typeOptions)
+    cy.get('#field1e').type('Field 1e text value', typeOptions)
+
+    cy.contains('Next').click()
+
+    cy.log('Second page')
+    cy.contains('h1', 'Book Hotel 2')
+    cy.get('form')
+      .getComponent()
+      .its('state')
+      .should('deep.equal', startOfSecondPageState)
+  })
+
+  it('second page', () => {
+    cy.get('form').getComponent().invoke('setState', startOfSecondPageState)
+
+    cy.log('Second page')
+    cy.contains('h1', 'Book Hotel 2')
+    // start filling input fields on page 2
+    cy.get('#username').type('JoeSmith', typeOptions)
+    cy.get('#field2a').type('Field 2a text value', typeOptions)
+    cy.get('#field2b').type('Field 2b text value', typeOptions)
+    cy.get('#field2c').type('Field 2c text value', typeOptions)
+    cy.get('#field2d').type('Field 2d text value', typeOptions)
+    cy.get('#field2e').type('Field 2e text value', typeOptions)
+    cy.get('#field2f').type('Field 2f text value', typeOptions)
+    cy.get('#field2g').type('Field 2g text value', typeOptions)
+    cy.contains('Next').click()
+
+    cy.log('Third page')
+    cy.contains('h1', 'Book Hotel 3')
+
+    cy.get('form')
+      .getComponent()
+      .its('state')
+      .should('deep.equal', startOfThirdPageState)
+  })
+
+  it('third page', () => {
+    cy.get('form')
+      .getComponent()
+      .then((comp) => {
+        cy.spy(comp, 'handleSubmit').as('handleSubmit')
+      })
+      .invoke('setState', startOfThirdPageState)
+
+    cy.log('Third page')
+    cy.contains('h1', 'Book Hotel 3')
+
+    cy.get('#field3a').type('Field 3a text value', typeOptions)
+    cy.get('#field3b').type('Field 3b text value', typeOptions)
+    cy.get('#field3c').type('Field 3c text value', typeOptions)
+    cy.get('#field3d').type('Field 3d text value', typeOptions)
+    cy.get('#field3e').type('Field 3e text value', typeOptions)
+    cy.get('#field3f').type('Field 3f text value', typeOptions)
+    cy.get('#field3g').type('Field 3g text value', typeOptions)
+
+    cy.contains('button', 'Sign up').click()
+    cy.contains('button', 'Thank you')
+
+    cy.get('form').parent().getComponent().its('state').should('deep.include', {
+      submitted: true,
+      username: 'JoeSmith',
+    })
+
+    // the spy is called once
+    cy.get('@handleSubmit').should('be.calledOnce')
+  })
+})
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class MasterForm extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       currentStep: 1,
       email: '',
-      username: ''
+      username: '',
     }
 
     if (window.Cypress) {
@@ -15,22 +15,23 @@ class MasterForm extends React.Component {
     }
   }
 
-  handleChange = event => {
+  handleChange = (event) => {
     const { name, value } = event.target
     this.setState({
-      [name]: value
+      [name]: value,
     })
   }
 
-  handleSubmit = event => {
+  handleSubmit = (event) => {
     event.preventDefault()
 
     console.log('submitting state', this.state)
+    debugger
 
     const { email, username } = this.state
 
     this.setState({
-      submitted: true
+      submitted: true,
     })
 
     alert(`Your registration detail: \n
@@ -44,7 +45,7 @@ class MasterForm extends React.Component {
     let currentStep = this.state.currentStep
     currentStep = currentStep >= 2 ? 3 : currentStep + 1
     this.setState({
-      currentStep: currentStep
+      currentStep: currentStep,
     })
   }
 
@@ -52,20 +53,20 @@ class MasterForm extends React.Component {
     let currentStep = this.state.currentStep
     currentStep = currentStep <= 1 ? 1 : currentStep - 1
     this.setState({
-      currentStep: currentStep
+      currentStep: currentStep,
     })
   }
 
   /*
    * the functions for our button
    */
-  previousButton () {
+  previousButton() {
     let currentStep = this.state.currentStep
     if (currentStep !== 1) {
       return (
         <button
-          className='btn btn-secondary'
-          type='button'
+          className="btn btn-secondary"
+          type="button"
           onClick={this._prev}
         >
           Previous
@@ -75,13 +76,13 @@ class MasterForm extends React.Component {
     return null
   }
 
-  nextButton () {
+  nextButton() {
     let currentStep = this.state.currentStep
     if (currentStep < 3) {
       return (
         <button
-          className='btn btn-primary float-right'
-          type='button'
+          className="btn btn-primary float-right"
+          type="button"
           onClick={this._next}
         >
           Next
@@ -91,49 +92,51 @@ class MasterForm extends React.Component {
     return null
   }
 
-  render () {
+  render() {
     return (
       <React.Fragment>
-        <h1>Book Hotel {this.state.currentStep}/3</h1>
-        <p>All fields are required</p>
+        <div className="app">
+          <h1>Book Hotel {this.state.currentStep}/3</h1>
+          <p>All fields are required</p>
 
-        <form onSubmit={this.handleSubmit}>
-          {/*
+          <form onSubmit={this.handleSubmit}>
+            {/*
         render the form steps and pass required props in
       */}
-          <Step1
-            currentStep={this.state.currentStep}
-            handleChange={this.handleChange}
-            email={this.state.email}
-          />
-          <Step2
-            currentStep={this.state.currentStep}
-            handleChange={this.handleChange}
-            username={this.state.username}
-          />
-          <Step3
-            currentStep={this.state.currentStep}
-            handleChange={this.handleChange}
-            password={this.state.password}
-            submitted={this.state.submitted}
-          />
-          {this.previousButton()}
-          {this.nextButton()}
-        </form>
+            <Step1
+              currentStep={this.state.currentStep}
+              handleChange={this.handleChange}
+              email={this.state.email}
+            />
+            <Step2
+              currentStep={this.state.currentStep}
+              handleChange={this.handleChange}
+              username={this.state.username}
+            />
+            <Step3
+              currentStep={this.state.currentStep}
+              handleChange={this.handleChange}
+              password={this.state.password}
+              submitted={this.state.submitted}
+            />
+            {this.previousButton()}
+            {this.nextButton()}
+          </form>
+        </div>
       </React.Fragment>
     )
   }
 }
 
-const Field = props => {
+const Field = (props) => {
   return (
     <React.Fragment>
       <label htmlFor={props.name}>Field {props.name}</label>
       <input
-        className='form-control'
+        className="form-control"
         id={props.name}
         name={props.name}
-        type='text'
+        type="text"
         placeholder={'Enter value for ' + props.name}
         value={props.field1}
         onChange={props.handleChange}
@@ -142,66 +145,66 @@ const Field = props => {
   )
 }
 
-function Step1 (props) {
+function Step1(props) {
   if (props.currentStep !== 1) {
     return null
   }
   return (
-    <div className='form-group'>
-      <Field name='first' handleChange={props.handleChange} />
-      <Field name='last' handleChange={props.handleChange} />
-      <Field name='email' handleChange={props.handleChange} />
-      <Field name='field1a' handleChange={props.handleChange} />
-      <Field name='field1b' handleChange={props.handleChange} />
-      <Field name='field1c' handleChange={props.handleChange} />
-      <Field name='field1d' handleChange={props.handleChange} />
-      <Field name='field1e' handleChange={props.handleChange} />
+    <div className="form-group">
+      <Field name="first" handleChange={props.handleChange} />
+      <Field name="last" handleChange={props.handleChange} />
+      <Field name="email" handleChange={props.handleChange} />
+      <Field name="field1a" handleChange={props.handleChange} />
+      <Field name="field1b" handleChange={props.handleChange} />
+      <Field name="field1c" handleChange={props.handleChange} />
+      <Field name="field1d" handleChange={props.handleChange} />
+      <Field name="field1e" handleChange={props.handleChange} />
     </div>
   )
 }
 
-function Step2 (props) {
+function Step2(props) {
   if (props.currentStep !== 2) {
     return null
   }
 
   return (
-    <div className='form-group'>
-      <Field name='username' handleChange={props.handleChange} />
-      <Field name='field2a' handleChange={props.handleChange} />
-      <Field name='field2b' handleChange={props.handleChange} />
-      <Field name='field2c' handleChange={props.handleChange} />
-      <Field name='field2d' handleChange={props.handleChange} />
-      <Field name='field2e' handleChange={props.handleChange} />
-      <Field name='field2f' handleChange={props.handleChange} />
-      <Field name='field2g' handleChange={props.handleChange} />
+    <div className="form-group">
+      <Field name="username" handleChange={props.handleChange} />
+      <Field name="field2a" handleChange={props.handleChange} />
+      <Field name="field2b" handleChange={props.handleChange} />
+      <Field name="field2c" handleChange={props.handleChange} />
+      <Field name="field2d" handleChange={props.handleChange} />
+      <Field name="field2e" handleChange={props.handleChange} />
+      <Field name="field2f" handleChange={props.handleChange} />
+      <Field name="field2g" handleChange={props.handleChange} />
     </div>
   )
 }
 
-function Step3 (props) {
+function Step3(props) {
   if (props.currentStep !== 3) {
     return null
   }
 
   const button = props.submitted ? (
-    <button className='btn btn-success btn-block' disabled>
+    <button className="btn btn-success btn-block" disabled>
       Thank you
     </button>
   ) : (
-    <button className='btn btn-block'>Sign up</button>
+    <button className="btn btn-block">Sign up</button>
   )
 
   return (
     <React.Fragment>
-      <div className='form-group'>
-        <Field name='field3a' handleChange={props.handleChange} />
-        <Field name='field3b' handleChange={props.handleChange} />
-        <Field name='field3c' handleChange={props.handleChange} />
-        <Field name='field3d' handleChange={props.handleChange} />
-        <Field name='field3e' handleChange={props.handleChange} />
-        <Field name='field3f' handleChange={props.handleChange} />
-        <Field name='field3g' handleChange={props.handleChange} />
+      <div className="form-group">
+        <Field name="field3a" handleChange={props.handleChange} />
+        <Field name="field3b" handleChange={props.handleChange} />
+        <Field name="field3c" handleChange={props.handleChange} />
+        <Field name="field3d" handleChange={props.handleChange} />
+        <Field name="field3e" handleChange={props.handleChange} />
+        <Field name="field3f" handleChange={props.handleChange} />
+        <Field name="field3g" handleChange={props.handleChange} />
       </div>
       {button}
     </React.Fragment>
